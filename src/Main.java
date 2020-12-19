3ed5270397daf0de8aa806ac781616b004e14dd4
kek <kek@lord.com>
2020-12-19 11:37:34 +0300

    ---

--- src/Main.java
+++ src/Main.java
@@ -5,45 +5,43 @@ public class Main {
 
     public static void main(String[] args) {
 
-        ArrayDeque<Card> firstPlayerDeck = new ArrayDeque<>();
-        ArrayDeque<Card> secondPlayerDeck = new ArrayDeque<>();
-        final int deckSize = 5;
+        ArrayDeque<Card> deck1 = new ArrayDeque<>();
+        ArrayDeque<Card> deck2 = new ArrayDeque<>();
+        final int size = 5;
 
         Scanner scanner = new Scanner(System.in);
 
-        for (int i = 0; i < deckSize; ++i) {
+        for (int i = 0; i < size; ++i) {
             Card card = new Card(scanner.nextInt());
-            firstPlayerDeck.addLast(card);
+            deck1.add(card);
         }
 
-        for (int i = 0; i < deckSize; ++i) {
+        for (int i = 0; i < size; ++i) {
             Card card = new Card(scanner.nextInt());
-            secondPlayerDeck.addLast(card);
+            deck2.add(card);
         }
 
 
-        Card firstPlayerCard, secondPlayerCard;
         int turns = 0;
-        final int MAX_TURNS = 106;
         String winner = "botva";
 
-        while (!firstPlayerDeck.isEmpty() && !secondPlayerDeck.isEmpty() && turns <= MAX_TURNS) {
-            firstPlayerCard = firstPlayerDeck.pollFirst();
-            secondPlayerCard = secondPlayerDeck.pollFirst();
-            if (firstPlayerCard.isGreater(secondPlayerCard)) {
-                firstPlayerDeck.addLast(firstPlayerCard);
-                firstPlayerDeck.addLast(secondPlayerCard);
+        while (!deck1.isEmpty() && !deck2.isEmpty() && turns <= 106) {
+            Card card1 = deck1.pollFirst();
+            Card card2 = deck2.pollFirst();
+            if (card1.isGreater(card2)) {
+                deck1.addLast(card1);
+                deck1.addLast(card2);
                 winner = "first";
             }
             else {
-                secondPlayerDeck.addLast(firstPlayerCard);
-                secondPlayerDeck.addLast(secondPlayerCard);
+                deck2.addLast(card1);
+                deck2.addLast(card2);
                 winner = "second";
             }
             ++turns;
         }
 
-        if (turns > MAX_TURNS)
+        if (turns > 106)
             System.out.println("botva");
         else
             System.out.println(winner + " " + turns);
